c03c8a72d0c3e68b6152c6f65155664e695e590e
mahatmaArrayyan <muh.mahatma@gmail.com>
2024-10-14 11:54:14 +0800

    feat: factories for kencelng and profiles

--- database/factories/KenclengFactory.php
+++ database/factories/KenclengFactory.php
@@ -17,11 +17,8 @@ class KenclengFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'no_kencleng' => fake()->randomNumber(8),
+            'qr_image' => '/storage/gambar/kencleng/' . fake()->randomNumber(2) . '.png',
         ];
     }
 }
