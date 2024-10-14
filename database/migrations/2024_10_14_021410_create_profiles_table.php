4121c55fc349dfb2e05ad72a50f8830b795676af
perdidev <perdidev@gmail.com>
2024-10-14 11:05:34 +0800

    feat: create kencleng, profile, distribusiKencleng migration

---
+++ database/migrations/2024_10_14_021410_create_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama', 255);
+            $table->date('tgl_lahir');
+            $table->enum('kelamin', ['L', 'P']);
+            $table->string('pekerjaan', 100);
+            $table->string('alamat', 255);
+            $table->string('kelurahan', 100);
+            $table->string('kecamatan', 100);
+            $table->string('kabupaten', 100);
+            $table->string('provinsi', 100);
+            $table->string('no_hp', 15);
+            $table->string('no_wa', 15);
+            $table->string('poto', 100);
+            $table->string('poto_ktp', 100);
+            $table->string('group', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
