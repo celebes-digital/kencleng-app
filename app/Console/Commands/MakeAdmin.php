93faaba3b4c45a262f4ce6de47e4c77ad871f36e
perdidev <perdidev@gmail.com>
2024-10-21 22:55:56 +0800

    feat: command to create user admin

---
+++ app/Console/Commands/MakeAdmin.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User;
+use Illuminate\Console\Command;
+use Illuminate\Contracts\Console\PromptsForMissingInput;
+
+class MakeAdmin extends Command implements PromptsForMissingInput
+{
+    protected $signature = 'make:user-admin {email} {--P|password=}';
+
+    protected $description = 'Command to create admin user';
+
+    protected function promptForMissingArgumentsUsing()
+    {
+        return [
+            'email' => 'Enter your email to submit as admin'
+        ];
+    }
+
+    public function handle()
+    {
+        if (!$this->argument('email')) {
+            $this->error('Email is required.');
+        }
+        
+        if(!$this->option('password')) {
+            $password = $this->secret('Enter your password');
+        }
+
+        if (User::where('email', $this->argument('email'))->exists()) {
+            $this->newLine();
+            $this->error('User already exists.');
+            return;
+        }
+
+        $this->info('Create user as admin...');
+        $password = $this->option('password') ?? $password;
+        User::create([
+            'email'     => $this->argument('email'),
+            'password'  => bcrypt($password),
+            'is_admin'  => true,
+            'is_active' => true,
+            'email_verified_at' => now(),
+        ]);
+
+        $this->newLine();
+        $this->info('Success to create admin user.');
+    }
+}
