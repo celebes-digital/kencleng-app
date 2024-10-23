da8cb24a012718df441b2d090a561a2f96c0360f
perdidev <perdidev@gmail.com>
2024-10-23 17:45:00 +0800

    refactor: Refactor TagLokasi page and form
    - Refactor TagLokasi page and form to improve code organization and readability
    - Move imports to separate lines for better readability
    - Reformat code to adhere to coding standards
    - Update form actions label to 'Simpan'
    - Add validation check for No. Kencleng
    - Update error notification message for invalid No. Kencleng
    - Update success notification message for saving location

--- app/Filament/Pages/TagLokasi.php
+++ app/Filament/Pages/TagLokasi.php
@@ -2,22 +2,24 @@
 
 namespace App\Filament\Pages;
 
-use App\Filament\Components\ScannerQrCode;
-use App\Models\DistribusiKencleng;
 use App\Models\Kencleng;
-use Filament\Forms\Components\Select;
-use Filament\Forms\Components\TextInput;
+use App\Models\DistribusiKencleng;
+
+use App\Filament\Components\ScannerQrCode;
+
+use Filament\Pages\Page;
+use Filament\Actions\Action;
+use Filament\Support\Exceptions\Halt;
+use Filament\Notifications\Notification;
+
 use Filament\Forms\Concerns\InteractsWithForms;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Form;
 use Filament\Forms\Set;
-use Filament\Notifications\Notification;
-use Filament\Pages\Page;
-use Closure;
-use Filament\Actions\Action;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Fieldset;
+use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Split;
-use Filament\Support\Exceptions\Halt;
 
 class TagLokasi extends Page implements HasForms
 {
@@ -25,7 +27,7 @@ class TagLokasi extends Page implements HasForms
 
     public ?array $data = [];
 
-    protected static string $view = 'filament.pages.tag-lokasi';
+    protected static string  $view              = 'filament.pages.tag-lokasi';
     protected static ?string $modelLabel        = 'Tag Lokasi';
     protected static ?string $label             = 'Tag Lokasi';
     protected static ?string $navigationIcon    = 'heroicon-o-cube';
@@ -40,35 +42,42 @@ class TagLokasi extends Page implements HasForms
 
     public function form(Form $form): Form
     {
-        return $form->schema([
-            Split::make([
-                ScannerQrCode::make('scanner')
-                    ->label('')
-                    ->live()
-                    ->afterStateUpdated(function (Set $set, $state) {
-                        $kencleng = Kencleng::where('no_kencleng', $state)->first();
-                        Notification::make()
-                            ->title('Kencleng ' . $kencleng->no_kencleng  . ' ditemukan')
-                            ->success()
-                            ->send();
+        return $form
+            ->schema([
+                Split::make([
+                    ScannerQrCode::make('scanner')
+                        ->label('')
+                        ->live()
+                        ->afterStateUpdated(function (Set $set, $state) {
+                            $kencleng = Kencleng::where('no_kencleng', $state)->first();
 
-                        $set('kencleng_id', $state);
-                    }),
-                Fieldset::make('')
-                    ->schema([
-                        TextInput::make('kencleng_id')
-                            ->label('No. Kencleng')
-                            ->disabled()
-                            ->required()
-                            ->columnSpanFull(),
-                        TextInput::make('latitude')
-                            ->disabled(),
-                        TextInput::make('longitude')
-                            ->disabled(),
-                    ])
+                            Notification::make()
+                                ->title('Kencleng ' . $kencleng->no_kencleng  . ' ditemukan')
+                                ->success()
+                                ->send();
+
+                            $set('kencleng_id', $kencleng->id);
+                        }),
+                    Fieldset::make('')
+                        ->schema([
+                            Select::make('kencleng_id')
+                                ->label('No. Kencleng')
+                                ->options(
+                                    Kencleng::all()->pluck('no_kencleng', 'id')
+                                )
+                                ->disabled()
+                                ->required()
+                                ->hint('Scan QR Code Kencleng')
+                                ->columnSpanFull(),
+                            TextInput::make('latitude')
+                                ->disabled()
+                                ->required(),
+                            TextInput::make('longitude')
+                                ->disabled()
+                                ->required(),
+                        ])
+                ])->from('md'),
             ])
-            ->from('md'),
-        ])
             ->statePath('data')
             ->columns(1);
     }
@@ -77,17 +86,18 @@ class TagLokasi extends Page implements HasForms
     {
         return [
             Action::make('save')
-                ->label(__('filament-panels::resources/pages/edit-record.form.actions.save.label'))
+                ->label('Simpan')
                 ->submit('save'),
         ];
     }
 
 
-    public function checkNoKencleng($noKencleng)
+    public function checkNoKencleng($noKencleng): bool
     {
         $kencleng = Kencleng::where('no_kencleng', $noKencleng)->first();
 
-        if (!$kencleng) {
+        if (!$kencleng) 
+        {
             Notification::make()
                 ->title('Kencleng ' . $noKencleng  . ' tidak terdaftar')
                 ->danger()
@@ -102,13 +112,13 @@ class TagLokasi extends Page implements HasForms
     public function save()
     {
         try {
-            $distribusiKencleng = DistribusiKencleng::whereHas('kencleng', function ($query) {
-                $query->where('no_kencleng', $this->data['kencleng_id']);
-            })->whereNull('tgl_distribusi')->first();
+            $distribusiKencleng = DistribusiKencleng::where('kencleng_id', $this->data['kencleng_id'])
+                ->whereNull('tgl_distribusi')
+                ->first();
 
             if (empty($distribusiKencleng)) {
                 Notification::make()
-                    ->title('Distribusi Kencleng tidak ditemukan atau belum didistribusikan')
+                    ->title('Kencleng tidak tersedia, hubungi admin untuk informasi lebih lanjut')
                     ->danger()
                     ->send();
                 return;
@@ -119,14 +129,14 @@ class TagLokasi extends Page implements HasForms
                 'geo_lat'        => $this->data['latitude'],
                 'geo_long'       => $this->data['longitude'],
             ]);
-            
+
         } catch (Halt $e) {
             return;
         }
 
         Notification::make()
             ->success()
-            ->title(__('filament-panels::resources/pages/edit-record.notifications.saved.title'))
+            ->title('Lokasi Kencleng berhasil ditandai')
             ->send();
     }
 }
