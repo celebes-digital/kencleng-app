52721f959be1d906424a8359a58cbdd3fbc0ef10
perdidev <perdidev@gmail.com>
2024-10-25 17:19:21 +0800

    fix: update status kencleng when distributed

--- app/Filament/Resources/DistribusiKenclengResource/Pages/CreateDistribusiKencleng.php
+++ app/Filament/Resources/DistribusiKenclengResource/Pages/CreateDistribusiKencleng.php
@@ -2,15 +2,31 @@
 
 namespace App\Filament\Resources\DistribusiKenclengResource\Pages;
 
-use App\Filament\Resources\DistribusiKenclengResource;
 use App\Models\Kencleng;
-use Filament\Actions;
+use App\Filament\Resources\DistribusiKenclengResource;
+
 use Filament\Notifications\Notification;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateDistribusiKencleng extends CreateRecord
 {
     protected static string $resource = DistribusiKenclengResource::class;
+
+    protected function beforeCreate($record)
+    {
+        $kencleng = Kencleng::where('no_kencleng', $record->no_kencleng)->first();
+        
+        if($kencleng->status == 1 ) {
+            Notification::make()
+                ->title('Kencleng ' . $record->no_kencleng . ' sedang didistribusikan')
+                ->danger()
+                ->send();
+
+            $this->halt();
+        } 
+        $kencleng->status = 1;
+        $kencleng->save();
+    }
     
     public function checkNoKencleng($noKencleng)
     {
