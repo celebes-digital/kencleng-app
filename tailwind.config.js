5c00c7807300b4b2b7a91b27aa4b9d7d6df35e61
perdidev <perdidev@gmail.com>
2024-11-15 13:12:28 +0800

    fix: update tailwind config for panel filament and livewire

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
+import preset from './vendor/filament/support/tailwind.config.preset'
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    presets: [preset],
     content: [
         "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
         "./storage/framework/views/*.php",
         "./resources/views/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
+        "./app/Filament/**/*.php",
 
         // Activity timeline
         "./vendor/jaocero/activity-timeline/resources/views/**/*.blade.php",
