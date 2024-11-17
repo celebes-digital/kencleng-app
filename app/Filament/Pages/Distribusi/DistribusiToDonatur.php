0510fe1f37663390006dcb13cefea28b34f4d19c
perdidev <perdidev@gmail.com>
2024-11-17 10:26:07 +0800

    feat: add action to show a qr pendaftaran for donatur (Just action)

--- app/Filament/Pages/Distribusi/DistribusiToDonatur.php
+++ app/Filament/Pages/Distribusi/DistribusiToDonatur.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Pages\Distribusi;
 
+use Filament\Actions\Action;
 use Filament\Pages\Page;
 
 class DistribusiToDonatur extends Page
@@ -13,6 +14,16 @@ class DistribusiToDonatur extends Page
     protected static ?string $slug              = 'distribusi/donatur';
     protected static ?string $navigationGroup   = 'Distribusi';
     protected static ?int    $navigationSort    = 3;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Action::make('open_qr')
+                ->label('QR Pendaftaran')
+                ->icon('heroicon-o-viewfinder-circle')
+                ->modal()
+        ];
+    }
 
     protected static ?string $navigationLabel   = 'Ke Donatur';
 }
