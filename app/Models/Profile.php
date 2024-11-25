1e82944394b17ddce23b3961fc814b68a7ef5deb
perdidev <perdidev@gmail.com>
2024-11-25 18:02:04 +0800

    feat: enhance Profile model to include cabang_id and wilayah_id with automatic assignment from area_id

--- app/Models/Profile.php
+++ app/Models/Profile.php
@@ -29,7 +29,17 @@ class Profile extends Model
         'group',
         'user_id',
         'area_id',
+        'cabang_id',
+        'wilayah_id',
     ];
+
+    protected static function booted()
+    {
+        static::creating(function ($profile) {
+            $profile->cabang_id = Area::find($profile->area_id)->cabang_id ?? null;
+            $profile->wilayah_id = Cabang::find($profile->cabang_id)->wilayah_id ?? null;
+        });
+    }
 
     public function user(): BelongsTo
     {
@@ -39,5 +49,15 @@ class Profile extends Model
     public function area(): BelongsTo
     {
         return $this->belongsTo(Area::class);
+    }
+
+    public function cabang(): BelongsTo
+    {
+        return $this->belongsTo(Cabang::class);
+    }
+
+    public function wilayah(): BelongsTo
+    {
+        return $this->belongsTo(Wilayah::class);
     }
 }
