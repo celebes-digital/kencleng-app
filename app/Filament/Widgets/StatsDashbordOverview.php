71e7875ea953611fc484be9f62f394e9a83e6f35
perdidev <perdidev@gmail.com>
2024-11-25 18:28:25 +0800

    feat: enhance StatsDashboardOverview widget to improve statistics retrieval and filtering based on user roles

--- app/Filament/Widgets/StatsDashbordOverview.php
+++ app/Filament/Widgets/StatsDashbordOverview.php
@@ -2,91 +2,99 @@
 
 namespace App\Filament\Widgets;
 
-use Illuminate\Support\Carbon;
-
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
 use App\Models\DistribusiKencleng;
 use App\Models\Infaq;
 use App\Models\Profile;
+
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Number;
 
 class StatsDashbordOverview extends BaseWidget
 {
+    private $startOfMonth;
+    private $user;
+
+    public function __construct()
+    {
+        $this->user         = Auth::user()->admin;
+        $this->startOfMonth = Carbon::now()->startOfMonth();
+    }
 
     public static function canView(): bool
     {
         return Auth::user()->is_admin;
     }
 
+
     protected function getStats(): array
     {
-        $now                = Carbon::now();
-        $startOfMonth       = $now->startOfMonth();
-
-        $totalPemasukanKencleng         = Infaq::sum('jumlah_donasi');
-
-        $totalDistribusiKencleng        = DistribusiKencleng::whereNotNull('tgl_distribusi')
-                                                            ->count();
-
-        $distribusiKenclengBulanIni     = DistribusiKencleng::where
-                                            (
-                                                'tgl_distribusi', '>=', $now->startOfMonth()
-                                            )
-                                            ->count();
-        
-        $totalPengguna                  = Profile::all()->count();
-        
-        $totalPenggunaBulanIni          = Profile::where
-                                            (
-                                                'created_at', '>=', $now->startOfMonth()
-                                            )
-                                            ->count();
-
-
-        $currentMonthSum                = Infaq::where
-                                            (
-                                                'tgl_transaksi', '>=', $startOfMonth
-                                            )
-                                            ->sum('jumlah_donasi');
-
         return [
-            Stat::make
-                (
-                    'Total Pemasukan Kencleng', 
-                    $this->formatRupiah($totalPemasukanKencleng)
-                )
-                ->description
-                (
-                    $this->formatRupiah($currentMonthSum) . ' bulan ini'
-                )
-                ->descriptionIcon('heroicon-m-banknotes')
-                ->color('success'),
-
-            Stat::make
-                (
-                    'Total Distribusi Kencleng', 
-                    $totalDistribusiKencleng . ' Kencleng'
-                )
-                ->description($distribusiKenclengBulanIni . ' kencleng bulan ini')
-                ->descriptionIcon('heroicon-m-cube')
-                ->color('info'),
-                
-            Stat::make
-                (
-                    'Total Pengguna', 
-                    $totalPengguna . ' Pengguna'
-                )
-                ->description($totalPenggunaBulanIni . ' pengguna bulan ini')
-                ->descriptionIcon('heroicon-m-user-plus')
-                ->color('warning'),
+            $this->getStatPemasukan(),
+            $this->getStatDistribusi(),
+            $this->getStatDonatur(),
         ];
     }
 
+    private function getStatPemasukan()
+    {
+        $query      = Infaq::query();
+        $query      = $this->filterData($query);
 
-    // Fungsi format ke uang, jadi kalau satu 1980200 menjadi Rp 1,98 Jt
+        $total      = $query->sum('jumlah_donasi');
+        $bulanIni   = $query->where('tgl_transaksi', '>=', $this->startOfMonth)->sum('jumlah_donasi');
+
+        return Stat::make('Total Pemasukan Kencleng', $this->formatRupiah($total))
+            ->description($this->formatRupiah($bulanIni) . ' bulan ini')
+            ->descriptionIcon('heroicon-m-banknotes')
+            ->color('success');
+    }
+
+    private function getStatDistribusi()
+    {
+        $query      = DistribusiKencleng::query();
+        $query      = $this->filterData($query);
+
+        $total      = $query->get()->count();
+        $bulanIni   = $query->where('tgl_distribusi', '>=', $this->startOfMonth)->count();
+
+        return Stat::make('Total Distribusi Kencleng', $total . ' Kencleng')
+            ->description($bulanIni . ' kencleng bulan ini')
+            ->descriptionIcon('heroicon-m-cube')
+            ->color('info');
+    }
+
+    private function getStatDonatur()
+    {
+        $query      = Profile::where('group', 'donatur');
+        $query      = $this->filterData($query);
+
+        $total      = $query->count();
+        $bulanIni   = $query->where('created_at', '>=', $this->startOfMonth)->count();
+
+        return Stat::make('Total Donatur', $total . ' Donatur')
+            ->description($bulanIni . ' donatur bulan ini')
+            ->descriptionIcon('heroicon-m-user-plus')
+            ->color('warning');
+    }
+
+    private function filterData($query)
+    {
+        if($this->user->level === 'supervisor') 
+            return $query->where('area_id', $this->user->area_id);
+
+        if($this->user->level === 'admin' || $this->user->level === 'manajer') 
+            return $query->where('cabang_id', $this->user->cabang_id);
+
+        if($this->user->level === 'admin_wilayah' || $this->user->level === 'direktur') 
+            return $query->where('wilayah_id', $this->user->wilayah_id);
+        
+        return $query;
+    }
+
+    // Fungsi format ke uang, jadi kalau 1980200 menjadi Rp 1,98 Jt
     private function formatRupiah($amount)
     {
         if ($amount >= 1000000000) {
